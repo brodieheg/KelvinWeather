bd487eb6e3c67fa23bb0d582d9b677691c1d2fa5
Brodie Heginbotham <111663689+brodieheg@users.noreply.github.com>
2023-05-15 14:37:58 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+const kelvin = 293;
+// today's forecast
+var celsius = kelvin - 273;
+// convert to celsius
+farenheit = celsius * (9/5) + 32;
+// convert to farenheit
+farenheit = Math.floor(farenheit);
+console.log ("The Temperature is ", + farenheit, "degrees Farenheit.");
